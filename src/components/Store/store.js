58cabb6c71603ef0b2adf65053e445384c9a9c30
InbalGam <inbalgam@post.bgu.ac.il>
2023-05-16 15:13:31 +0300

    removed communitySlice

--- src/components/Store/store.js
+++ src/components/Store/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postsReducer from './postsSlice';
-import communityReducer from './communitySlice';
 
 export default configureStore({
   reducer: {
-    posts: postsReducer,
-    community: communityReducer
+    posts: postsReducer
   },
 });
