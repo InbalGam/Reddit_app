b1b68ccfa31fda3376c2b14bd75464dd3aaad747
InbalGam <inbalgam@post.bgu.ac.il>
2023-05-17 11:12:03 +0300

    added timeAgo function

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,26 +4,63 @@ import styles from './styles/Post.css';
 
 /* Post example- 
                 author: el.data.author_fullname,
-                title: el.data.title,
-                url: el.data.url,
-                score: el.data.score,
-                numComments: el.data.num_comments,
-                viewCount: el.data.view_count,
-                ups: el.data.ups,
-                downs: el.data.downs,
-                selftext: el.data.selftext*/
+                    title: el.data.title,
+                    url: el.data.url,
+                    score: el.data.score,
+                    numComments: el.data.num_comments,
+                    viewCount: el.data.view_count,
+                    ups: el.data.ups,
+                    downs: el.data.downs,
+                    selftext: el.data.selftext,
+                    timeCreated: el.data.created_utc,
+                    comments: ['string1', 'string2'],
+                    thumbnail: el.data.thumbnail*/
 
 function Post(props) {
+    const timeAgo = (date) => {
+        const seconds = Math.floor((new Date() - date) / 1000);
+      
+        let interval = Math.floor(seconds / 31536000);
+        if (interval > 1) {
+          return interval + ' years ago';
+        }
+      
+        interval = Math.floor(seconds / 2592000);
+        if (interval > 1) {
+          return interval + ' months ago';
+        }
+      
+        interval = Math.floor(seconds / 86400);
+        if (interval > 1) {
+          return interval + ' days ago';
+        }
+      
+        interval = Math.floor(seconds / 3600);
+        if (interval > 1) {
+          return interval + ' hours ago';
+        }
+      
+        interval = Math.floor(seconds / 60);
+        if (interval > 1) {
+          return interval + ' minutes ago';
+        }
+      
+        if(seconds < 10) return 'just now';
+      
+        return Math.floor(seconds) + ' seconds ago';
+      };
+
+      
     return (
         props.posts.map((el, ind) => 
         <li key={ind}>
             <div className="post">
                 <h4>{el.title}</h4> 
-                {/* <img src={example} alt='example image for a post' styles={styles.img}/> */}
+                <img src={el.thumbnail} alt='post image' styles={styles.img}/>
                 <p>{el.author}</p>
                 <Comments comments={el.comments} numComments={el.numComments} />
                 <p>{el.ups - el.downs}</p>
-                <p>{el.timePosted}</p>
+                <p>{timeAgo(el.timeCreated)}</p>
             </div>
         </li>)
     );
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,17 +2,6 @@ import Post from './Post';
 import { useSelector } from 'react-redux';
 import { selectPosts } from './Store/postsSlice';
 
-/// example for a hard-coded post-
-/*{
-    headline: 'string',
-    image: img,
-    userName: name,
-    comments: ['string1', 'string2'],
-    likes: 100,
-    dislikes: 3,
-    timePosted: '12hrsago'
-}*/
-///
 function Posts() {
 
     const posts = useSelector(selectPosts);
