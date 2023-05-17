4d03299f9e10460193596606bb6768a1da955690
InbalGam <inbalgam@post.bgu.ac.il>
2023-05-17 11:12:57 +0300

    deleted example image

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,20 +1,6 @@
-import example from './images/example.jpg';
 import Comments from './Comments';
 import styles from './styles/Post.css';
 
-/* Post example- 
-                author: el.data.author_fullname,
-                    title: el.data.title,
-                    url: el.data.url,
-                    score: el.data.score,
-                    numComments: el.data.num_comments,
-                    viewCount: el.data.view_count,
-                    ups: el.data.ups,
-                    downs: el.data.downs,
-                    selftext: el.data.selftext,
-                    timeCreated: el.data.created_utc,
-                    comments: ['string1', 'string2'],
-                    thumbnail: el.data.thumbnail*/
 
 function Post(props) {
     const timeAgo = (date) => {
@@ -50,7 +36,7 @@ function Post(props) {
         return Math.floor(seconds) + ' seconds ago';
       };
 
-      
+
     return (
         props.posts.map((el, ind) => 
         <li key={ind}>
