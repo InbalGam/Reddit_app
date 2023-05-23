8325c7ed5254195cca4701eef1000cbabbc389bd
InbalGam <inbalgam@post.bgu.ac.il>
2023-05-23 09:34:18 +0300

    finished comments css

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -6,7 +6,12 @@ function Comment(props) {
     return (
             <li key={props.ind} className='commentList'>
                 <div className='commentContainer'>
-                    <p className='commentAuthor'>{props.el.author}</p> <p className='commentBody'>{props.el.body}</p> <p className='commentTime'>{timeAgo(props.el.timeCreated)}</p>
+                    <div className='AuthorTime'>
+                        <p className='commentAuthor'>{props.el.author}</p>
+                        <p className='commentTime'>{timeAgo(props.el.timeCreated)}</p>
+                    </div>
+                    <p className='commentBody'>{props.el.body}</p> 
+                    {/* <p className='commentTime'></p> */}
                 </div>
             </li>
     );
